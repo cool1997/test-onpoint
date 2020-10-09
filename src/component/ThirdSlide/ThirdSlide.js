6188def0043d9a63ddbf2aec6aec5e883f08e06e
cool1997 <palefire@gmail.com>
2020-10-09 16:59:36 +0300

    end

--- src/component/ThirdSlide/ThirdSlide.js
+++ src/component/ThirdSlide/ThirdSlide.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import styles from './ThirdSlide.module.scss'
-import { IceBrick } from '../components/IceBrick'
+import { IceBrick } from '../IceBrick/IceBrick'
 
 export const ThirdSlide = props => {
   return (
