8596c954555b94e8f4c5361a7d6e1cdee750db25
cool1997 <kirilov.vl.ol@gmail.com>
2020-10-10 10:44:15 +0300

    Updates

---
+++ precache-manifest.0c6b70bd8f1d4d0b6f90947431694eee.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ee625d7397e5c21ee35dff09ea3f11",
+    "url": "/test-onpoint/index.html"
+  },
+  {
+    "revision": "73a9469c8b0cb3adfa35",
+    "url": "/test-onpoint/static/css/main.a1df1b1a.chunk.css"
+  },
+  {
+    "revision": "9b1fdbb35c073c8b29f8",
+    "url": "/test-onpoint/static/js/2.5b91c3b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-onpoint/static/js/2.5b91c3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a9469c8b0cb3adfa35",
+    "url": "/test-onpoint/static/js/main.1ca882e7.chunk.js"
+  },
+  {
+    "revision": "3f836ff8f1daf3615314",
+    "url": "/test-onpoint/static/js/runtime-main.e35cd60c.js"
+  },
+  {
+    "revision": "3100f91bbd9e9ca9ecd00255cac7d11c",
+    "url": "/test-onpoint/static/media/GothamPro.3100f91b.ttf"
+  },
+  {
+    "revision": "792e5785377b31db37697448eba44281",
+    "url": "/test-onpoint/static/media/GothamProLight.792e5785.ttf"
+  },
+  {
+    "revision": "863b7dcd5ec2c3923122af25ce0f7e4c",
+    "url": "/test-onpoint/static/media/Lato-Medium.863b7dcd.ttf"
+  },
+  {
+    "revision": "3c6cfb1aebd888a0eb4c8fba94140fa6",
+    "url": "/test-onpoint/static/media/Lato-Semibold.3c6cfb1a.ttf"
+  },
+  {
+    "revision": "ef85b068e00bc909631d5167a4bbf86c",
+    "url": "/test-onpoint/static/media/NextPage.ef85b068.png"
+  },
+  {
+    "revision": "0e85934274fb5946345cbe5c7e6a9341",
+    "url": "/test-onpoint/static/media/first-block-bg.0e859342.png"
+  },
+  {
+    "revision": "9f01e4e563ed3af901ab94d71cc7f22f",
+    "url": "/test-onpoint/static/media/ice-brick-1.9f01e4e5.png"
+  },
+  {
+    "revision": "0ca0f3cdc68a81430a2cc8dc5c0e7519",
+    "url": "/test-onpoint/static/media/ice-brick-3.0ca0f3cd.png"
+  },
+  {
+    "revision": "8282b9aafc8a53e221766e904f96cd79",
+    "url": "/test-onpoint/static/media/ice-brick-4.8282b9aa.png"
+  },
+  {
+    "revision": "af0e17774e244e4f25904c577ccbfb77",
+    "url": "/test-onpoint/static/media/second-block-bg.af0e1777.png"
+  },
+  {
+    "revision": "cf3b29e7a33105c57f939df7ff1cb939",
+    "url": "/test-onpoint/static/media/slide-1-box.cf3b29e7.png"
+  },
+  {
+    "revision": "9a8818ca264ebbe0ee91736866a58d5f",
+    "url": "/test-onpoint/static/media/slide-1-ice-brick-2.9a8818ca.png"
+  },
+  {
+    "revision": "ccbe20d742fe1df76906c8d4bcad7c5d",
+    "url": "/test-onpoint/static/media/slide-1-ice-brick-3.ccbe20d7.png"
+  },
+  {
+    "revision": "9cbc2e422bf25e2877a7c028c0db7f33",
+    "url": "/test-onpoint/static/media/slide-2-box.9cbc2e42.png"
+  },
+  {
+    "revision": "9a8818ca264ebbe0ee91736866a58d5f",
+    "url": "/test-onpoint/static/media/slide-2-ice-brick-2.9a8818ca.png"
+  },
+  {
+    "revision": "2c2b8673ab63721485c5ac17062293ba",
+    "url": "/test-onpoint/static/media/slide-2-ice-brick-3.2c2b8673.png"
+  },
+  {
+    "revision": "de80b018b99a9b6aceadedc6dbb0d9fc",
+    "url": "/test-onpoint/static/media/slide-3-box.de80b018.png"
+  },
+  {
+    "revision": "341f82d38597010b764bfa4eeddec592",
+    "url": "/test-onpoint/static/media/slide-3-ice-brick-2.341f82d3.png"
+  },
+  {
+    "revision": "3f8f39ed85593de229a57ba0f4b0b8af",
+    "url": "/test-onpoint/static/media/slide-3-ice-brick-3.3f8f39ed.png"
+  },
+  {
+    "revision": "0d1bc5a7e44c31a15ee007a3ac0134cb",
+    "url": "/test-onpoint/static/media/third-block-bg.0d1bc5a7.png"
+  }
+]);
